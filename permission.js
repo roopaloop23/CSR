f3124be43897a7283a2189ebbce796403e38bb8a
thething <atillzcomputers@gmail.com>
2020-03-18 04:10:57 -0400

    Create permission.js
    permission.js Added to replace the IRC making it more setting friendly

---
+++ permission.js
@@ -0,0 +1,15 @@
+ exports.bot_logs = function(){
+  var getbotlogs ="689544434458820871"  // Chanel ID Bot Logs   
+  };
+
+ exports.server = function(){
+ // Allowed Servers 
+ var andS ='&& message.guild.id!='
+ var allservers ='"439858575624372235" andS "462007482001391616"'
+ // Comment out 303 - 304 in irc.js to allow all
+};
+
+exports.owner = function(){
+ // Owners  
+ var allowners =""
+};
