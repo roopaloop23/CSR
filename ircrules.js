c8c941a0f06ca45d480ad23b242c6a2ec31bae7e
roopaloop23 <63991390+roopaloop23@users.noreply.github.com>
2020-04-20 00:18:31 -0400

    Update ircrules.js
    Using “#” starts a comment in py.  Unconfirmed in js.

--- ircrules.js
+++ ircrules.js
@@ -1,10 +1,9 @@
-const code = `**\`\`1. be friendly:\`\`**
-this isnt a server,its multiple servers communicating, and there can be people who might take offense to what you type.
-**\`\`2. no advertising in #irc.\`\`**
-**\`\`3. no enforcing political viewpoints on people.\`\`**
-**\`\`4. no spamming in the irc channel.\`\`**\n**\`\`5. no nsfw in the irc channel\`\`**
-**\`\`6. Harassing CSR Staff is punishable with a Ban\`\`**
-**\`\`7. Any attempt to create and exploit loopholes will result in an unappealable Ban\`\`**
+const code = `**\`\`Be friendly:\`\`**
+Rewards bot is a feed from Hero Wars Mobile Discord server.
+**\`\`Heroes all across the dominion will receive the info posted to us.\`\`**
+**\`\`New users and other public information will be posted in our public channel.\`\`**
+**\`\`Failure to respect staff and others will be handled accordingly.\`\`**
+**\`\`To connect simply type the following command in your designated bot channel:\`\`**\n**\`\`c-connect #your-channel-name private HeroWarsRewards \`\`**
 `;
 
 module.exports = code;
