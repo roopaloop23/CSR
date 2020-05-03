efa691119ef18dc72b7af7ca020b4e1af40bdcc3
thething <atillzcomputers@gmail.com>
2020-05-03 02:31:22 -0400

    Update ircrules.js

--- ircrules.js
+++ ircrules.js
@@ -1,10 +1,32 @@
-const code = `**\`\`1. be friendly:\`\`**
-this isnt a server,its multiple servers communicating, and there can be people who might take offense to what you type.
-**\`\`2. no advertising in #irc.\`\`**
-**\`\`3. no enforcing political viewpoints on people.\`\`**
-**\`\`4. no spamming in the irc channel.\`\`**\n**\`\`5. no nsfw in the irc channel\`\`**
-**\`\`6. Harassing CSR Staff is punishable with a Ban\`\`**
-**\`\`7. Any attempt to create and exploit loopholes will result in an unappealable Ban\`\`**
+/*const code = `
+**\`\`___________________ஜ۩۞۩ஜ__________________\`\`**
+**\`\`=           HERO WARS REWARDS BOT          =\`\`**
+**\`\`___________________ஜ۩۞۩ஜ__________________\`\`**
+**\`\`                                           \xa0
+Rewards bot is a feed from Hero Wars Mobile \xa0\nDiscord server.                             \xa0
+Heroes all across the dominion will receive \xa0\nthe info posted to us.                      \xa0
+New users and other public information will \xa0\nbe posted in our public channel.            \xa0
+Failure to respect staff and others will be \xa0\nhandled accordingly.                        \xa0
+To connect simply type the following command\xa0\nin your designated bot channel: 'c-connect  \xa0
+#your-channel-name private HeroWarsRewards' \xa0
+Other commands 'c-stats' and 'c-testwebhooks'
+                                            \xa0
+=           HERO WARS REWARDS BOT           =
+___________________ஜ۩۞۩ஜ___________________\`\`**
 `;
+*/
+const code = `
+_________________....----++++ஜ۩۞۩ஜ++++----...._________________
+** \xa0= HERO WARS REWARDS BOT = **
 
-module.exports = code;
+Rewards bot is a feed from Hero Wars Mobile \xa0\nDiscord server.                             \xa0
+Heroes all across the dominion will receive \xa0\nthe info posted to us.                      \xa0
+New users and other public information will \xa0\nbe posted in our public channel.            \xa0
+Failure to respect staff and others will be \xa0\nhandled accordingly.                        \xa0
+To connect simply type the following command\xa0\nin your designated bot channel: \`c-connect   \xa0
+#your-channel-name private HeroWarsRewards\`  \xa0
+Other commands \`c-stats\` and \`c-testwebhooks\`
+                                            \xa0 
+=           HERO WARS REWARDS BOT           =
+______ ______ ______ ______ ______ஜ۩۞۩ஜ ______ ______ ______ ______ ______
+`;
