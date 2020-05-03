1ee08adcbdacad005e11dc3aa75b6305311a2c1e
thething <atillzcomputers@gmail.com>
2020-05-03 02:36:50 -0400

    Update ircrules.js

--- ircrules.js
+++ ircrules.js
@@ -19,14 +19,14 @@ const code = `
 _________________....----++++ஜ۩۞۩ஜ++++----...._________________
 ** \xa0= HERO WARS REWARDS BOT = **
 
-Rewards bot is a feed from Hero Wars Mobile \xa0\nDiscord server.                             \xa0
-Heroes all across the dominion will receive \xa0\nthe info posted to us.                      \xa0
-New users and other public information will \xa0\nbe posted in our public channel.            \xa0
-Failure to respect staff and others will be \xa0\nhandled accordingly.                        \xa0
-To connect simply type the following command\xa0\nin your designated bot channel: \`c-connect   \xa0
-#your-channel-name private HeroWarsRewards\`  \xa0
+Rewards bot is a feed from Hero Wars Mobile \nDiscord server. 
+Heroes all across the dominion will receive \nthe info posted to us. 
+New users and other public information will \nbe posted in our public channel. 
+Failure to respect staff and others will be \nhandled accordingly. 
+To connect simply type the following command\nin your designated bot channel: \`c-connect
+#your-channel-name private HeroWarsRewards\`  
 Other commands \`c-stats\` and \`c-testwebhooks\`
-                                            \xa0 
+
 =           HERO WARS REWARDS BOT           =
 ______ ______ ______ ______ ______ஜ۩۞۩ஜ ______ ______ ______ ______ ______
 `;
