f6d7fc93d9b35ec14abf29d9b4f91f3f0ffee776
thething <atillzcomputers@gmail.com>
2020-05-12 23:48:23 -0400

    Update last.js

--- commands/csr_public/last.js
+++ commands/csr_public/last.js
@@ -5,19 +5,18 @@ const last = new Command({
 	description: "Last Reward posted",
 	usage:'<prefix>last '});
 
-module.exports = last.execute((client, message, args)=>{ 
-	 
-	let target = client.channels.get(client.HWMChannel); // find the targeted channel 
-	 
+module.exports = last.execute(async(client, message, args)=>{ 
+
+	let target = client.channels.get(client.HWMChannel); // find the targeted channel 	 
 	if (!target) { 
 		message.channel.send("Hero Mobile Channel ID is not set.");
 		return
-	}
+	}		  
 		 
-	// send the last message to the initial channel    
-	setTimeout(async function() {
-	const msg = await target.fetchMessages({ limit: 3 }).then(msg => message.channel.send(
-	`Last 3 Rewards: \n> ${msg.array()[2].content}   \n> ${msg.array()[1].content}  \n> ${msg.first().content}  `)); 
-				      
-    });
-});
+	const msg = await 
+	target.fetchMessages({ limit: 3 })
+	.then(msg => message.channel.send(
+			`Last 3 Rewards: \n> ${msg.array()[2].content}   \n> ${msg.array()[1].content}  \n> ${msg.first().content}  `)); 
+			// send the last message to the initial channel    
+  }
+);
